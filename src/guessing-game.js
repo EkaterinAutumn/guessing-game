02baf9f65eb44268d5ea491cfd7ef0ecd5753bb7
Kate <kate@MacBook-Air-Kate.local>
2022-08-12 13:43:56 +0200

    Feat: add decision

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,28 @@
 class GuessingGame {
-    constructor() {}
+    min;
+    max;
+    value;
+    constructor() { }
 
     setRange(min, max) {
-
+      this.min = min;
+      this.max = max;
     }
 
     guess() {
-
+        this.value = Math.round((this.min + this.max) / 2);
+        return this.value;
+       
     }
 
     lower() {
-
+        this.max = this.value;
+        this.guess();
     }
 
     greater() {
-
+        this.min = this.value;
+        this.guess();
     }
 }
 
